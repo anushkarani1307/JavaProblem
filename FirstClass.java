f086a29d1043b4f5af543df57e25f460f82eb17a
Anushka Rani <studiousanushka@gmail.com>
2025-01-11 09:43:27 +0530

    commit

--- FirstClass.java
+++ FirstClass.java
@@ -1,7 +1,7 @@
 class FirstClass {
     public static void main(String[] args) {
         int a = 10;
-        int b = 5;
+        int b =
         int sum = (a * b) / (a - b);
         System.out.println(sum);
 
---
+++ multiply.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class multiply {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Enter First Number");
+        int a = sc.nextInt();
+        System.out.println(" Enter Second Number");
+        int b = sc.nextInt();
+        int c = a * b;
+        System.out.println("Multiplied Result =" + c);
+
+    }
+
+}
